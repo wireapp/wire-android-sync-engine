b6a1ec2df7d7413d917b969e4bf6cdefd94c2d5e
yeryomenkom <yeryomenkom@gmail.com>
2019-02-05 18:39:20 +0100

    add asset transformation service.

---
+++ zmessaging/src/main/scala/com/waz/service/assets2/AssetPoliciesService.scala
@@ -0,0 +1,42 @@
+/*
+ * Wire
+ * Copyright (C) 2016 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.waz.service.assets2
+
+import com.waz.model.Mime
+import com.waz.model.errors.AssetContentTooLargeError
+
+import scala.util.{Failure, Success, Try}
+
+//class AssetPoliciesServiceImpl(uriHelper: UriHelper, contentCache: RawAssetContentCache) {
+//
+//  def validate(content: Content): Try[Unit] = {
+//    for {
+//      mime <- content.getMime(uriHelper)
+//      size <- content.getSize(uriHelper)
+//
+//      shouldCheckSize = content match {
+//        case _: Content.AsBlob => true
+//        //we do not check size on case of an image asset because we downscale images
+//        case _ => !Mime.Image.supported.contains(mime) && mime != Mime.Image.Gif
+//      }
+//      _ <- if (shouldCheckSize && size > maxAssetSize()) Failure(AssetContentTooLargeError(size, maxAssetSize()))
+//      else Success(())
+//    } yield ()
+//  }
+//
+//}
