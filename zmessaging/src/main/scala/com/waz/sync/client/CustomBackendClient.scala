196cfebf4ad5e4cfa568f8e71946cf3fb4501da7
John Nguyen <johnxuannguyen@gmail.com>
2019-04-17 14:51:45 +0200

    Update response model

--- zmessaging/src/main/scala/com/waz/sync/client/CustomBackendClient.scala
+++ zmessaging/src/main/scala/com/waz/sync/client/CustomBackendClient.scala
@@ -1,3 +1,20 @@
+/*
+ * Wire
+ * Copyright (C) 2016 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.waz.sync.client
 
 import java.net.URL
@@ -30,12 +47,12 @@ class CustomBackendClientImpl(implicit httpClient: HttpClient)
 }
 
 object CustomBackendClient {
-  case class BackendConfigResponse(endpoints: EndPoints)
+  case class BackendConfigResponse(endpoints: EndPoints, title: String)
 
-  case class EndPoints(backendURL: String,
-                       backendWSURL: String,
-                       teamsURL: String,
-                       accountsURL: String,
-                       blackListURL: String,
-                       websiteURL: String)
+  case class EndPoints(backendURL: URL,
+                       backendWSURL: URL,
+                       blackListURL: URL,
+                       teamsURL: URL,
+                       accountsURL: URL,
+                       websiteURL: URL)
 }
