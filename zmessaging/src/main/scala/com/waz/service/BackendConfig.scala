44b9a022ce5c342b1d4a8517ecbfd99774f81972
John Nguyen <johnxuannguyen@gmail.com>
2019-04-29 15:42:31 +0200

    Fix/custom backend (#536)
    * Add missing endpoints to BackendConfig
    
    * Get blacklist url from config

--- zmessaging/src/main/scala/com/waz/service/BackendConfig.scala
+++ zmessaging/src/main/scala/com/waz/service/BackendConfig.scala
@@ -20,27 +20,37 @@ package com.waz.service
 import com.waz.sync.client.CustomBackendClient.BackendConfigResponse
 import com.waz.utils.wrappers.URI
 import com.waz.znet.ServerTrust
+import com.waz.znet2.http.Request.UrlCreator
 
 
 class BackendConfig(private var _environment: String,
                     private var _baseUrl: URI,
                     private var _websocketUrl: URI,
                     private var _blacklistHost: URI,
+                    private var _teamsUrl: URI,
+                    private var _accountsUrl: URI,
+                    private var _websiteUrl: URI,
                     val firebaseOptions: FirebaseOptions,
                     val pin: CertificatePin = ServerTrust.wirePin) {
 
   val pushSenderId: String = firebaseOptions.pushSenderId
-  
+
   def environment: String = _environment
   def baseUrl: URI = _baseUrl
   def websocketUrl: URI = _websocketUrl
   def blacklistHost: URI = _blacklistHost
+  def teamsUrl: URI = _teamsUrl
+  def accountsUrl: URI = _accountsUrl
+  def websiteUrl: URI = _websiteUrl
 
   def update(configResponse: BackendConfigResponse): Unit = {
     _environment = configResponse.title
     _baseUrl = URI.parse(configResponse.endpoints.backendURL.toString)
     _websocketUrl = URI.parse(configResponse.endpoints.backendWSURL.toString)
     _blacklistHost = URI.parse(configResponse.endpoints.blackListURL.toString)
+    _teamsUrl = URI.parse(configResponse.endpoints.teamsURL.toString)
+    _accountsUrl = URI.parse(configResponse.endpoints.accountsURL.toString)
+    _websiteUrl = URI.parse(configResponse.endpoints.websiteURL.toString)
   }
 }
 
@@ -49,12 +59,19 @@ object BackendConfig {
             baseUrl: String,
             websocketUrl: String,
             blacklistHost: String,
+            teamsUrl: String,
+            accountsUrl: String,
+            websiteUrl: String,
             firebaseOptions: FirebaseOptions,
-            pin: CertificatePin = ServerTrust.wirePin): BackendConfig = new BackendConfig(
+            pin: CertificatePin = ServerTrust.wirePin): BackendConfig =
+    new BackendConfig(
       environment,
       URI.parse(baseUrl),
       URI.parse(websocketUrl),
       URI.parse(blacklistHost),
+      URI.parse(teamsUrl),
+      URI.parse(accountsUrl),
+      URI.parse(websiteUrl),
       firebaseOptions,
       pin)
 }
