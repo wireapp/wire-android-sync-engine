f37a53bfd7db6e89819824893cfeb9c9eaa21c5f
John Nguyen <johnxuannguyen@gmail.com>
2019-05-06 16:19:18 +0200

    Convert json in UserSearchClient (#501)

--- zmessaging/src/main/scala/com/waz/service/UserService.scala
+++ zmessaging/src/main/scala/com/waz/service/UserService.scala
@@ -18,19 +18,19 @@
 package com.waz.service
 
 import com.waz.log.LogSE._
-import com.waz.content._
 import com.waz.log.BasicLogging.LogTag.DerivedLogTag
+import com.waz.content._
 import com.waz.model.AccountData.Password
 import com.waz.model.UserData.ConnectionStatus
 import com.waz.model.{AccentColor, _}
 import com.waz.service.EventScheduler.Stage
+import com.waz.service.UserSearchService.UserSearchEntry
 import com.waz.service.UserService._
 import com.waz.service.assets.AssetService
 import com.waz.service.assets.AssetService.RawAssetInput
 import com.waz.service.conversation.SelectedConversationService
 import com.waz.service.push.PushService
 import com.waz.sync.SyncServiceHandle
-import com.waz.sync.client.UserSearchClient.UserSearchEntry
 import com.waz.sync.client.{CredentialsUpdateClient, ErrorOr, UsersClient}
 import com.waz.threading.{CancellableFuture, SerialDispatchQueue, Threading}
 import com.waz.utils._
