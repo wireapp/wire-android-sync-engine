f57dcef50773c03dfce005a4ee8a37ac78537df5
Maciej Gorywoda <makingthematrix@gmail.com>
2019-06-25 15:45:34 +0200

    refactor: Decreasing the number of future calls in PushService
    This is an experimental PR.
    
    We have one device (Meng's) with a dev build connected to the Prod backend. The number of conversations and messages make the sync very slow,
    possibly even causing a variant of the stuck-on-sync bug (a race condition due to a lot of parallel syncs?). I decided to simplify `PushService`
    the best to my knowledge, limiting the number of created futures and async/awaits. It seems to work a bit faster, but:
    1. more through performance testing is needed, both on dev and internal builds
    2. the changes in code affect how the full sync is handled; this also needs to be tested.

--- zmessaging/src/main/scala/com/waz/service/push/PushService.scala
+++ zmessaging/src/main/scala/com/waz/service/push/PushService.scala
@@ -47,6 +47,7 @@ import org.threeten.bp.{Duration, Instant}
 import FCMNotification.{Fetched, FinishedPipeline, StartedPipeline}
 
 import scala.async.Async._
+import scala.collection.mutable
 import scala.concurrent.duration._
 import scala.concurrent.{Future, Promise}
 
@@ -111,8 +112,6 @@ class PushServiceImpl(selfUserId:           UserId,
 
   private val beDriftPref = prefs.preference(BackendDrift)
   override val beDrift = beDriftPref.signal.disableAutowiring()
-
-  private var fetchInProgress = Future.successful({})
 
   private lazy val idPref = userPrefs.preference(LastStableNotification)
 
@@ -189,25 +188,23 @@ class PushServiceImpl(selfUserId:           UserId,
     }
   }
 
-  wsPushService.notifications() { notifications =>
-    fetchInProgress =
-      if (fetchInProgress.isCompleted) storeNotifications(notifications)
-      else fetchInProgress.flatMap(_ => storeNotifications(notifications))
-  }
+  wsPushService.notifications(storeNotifications)
 
   wsPushService.connected().onChanged.map(WebSocketChange).on(dispatcher){ source =>
     verbose(l"sync history due to web socket change")
     syncHistory(source)
   }
 
-  private def storeNotifications(notifications: Seq[PushNotificationEncoded]): Future[Unit] =
-    Serialized.future(PipelineKey)(async {
-      await { fcmService.markNotificationsWithState(notifications.map(_.id).toSet, Fetched) }
-      await { notificationStorage.saveAll(notifications) }
-      val res = notifications.lift(notifications.lastIndexWhere(!_.transient))
-      if (res.nonEmpty)
-        await { idPref := res.map(_.id) }
-    })
+  private def storeNotifications(nots: Seq[PushNotificationEncoded]): Future[Unit] = synchronized {
+    if (nots.nonEmpty) {
+      for {
+        _   <- fcmService.markNotificationsWithState(nots.map(_.id).toSet, Fetched)
+        _   <- notificationStorage.saveAll(nots)
+        res = nots.lift(nots.lastIndexWhere(!_.transient))
+        _   <- if (res.nonEmpty) idPref := res.map(_.id) else Future.successful(())
+      } yield ()
+    } else Future.successful(())
+  }
 
   private def isOtrEventJson(ev: JSONObject) =
     ev.getString("type").equals("conversation.otr-message-add")
@@ -273,22 +270,22 @@ class PushServiceImpl(selfUserId:           UserId,
 
     def syncHistory(lastId: Option[Uid]): Future[Unit] =
       load(lastId, firstSync = lastId.isEmpty).future.flatMap {
-        case Results(nots, time, firstSync, historyLost) =>
-          if (firstSync) idPref := nots.headOption.map(_.id)
-          else
-            (for {
-              _ <- if (historyLost) sync.performFullSync().map(_ => onHistoryLost ! clock.instant()) else Future.successful({})
-              _ <- beDriftPref.mutate(v => time.map(clock.instant.until(_)).getOrElse(v))
-            } yield {
-              nots
-            }).flatMap(storeNotifications)
+        case Results(nots, _, true, _) =>
+          idPref := nots.headOption.map(_.id)
+        case Results(_, time, _, true) =>
+          sync.performFullSync()
+            .map(_ => onHistoryLost ! clock.instant())
+            .flatMap(_ => beDriftPref.mutate(v => time.map(clock.instant.until(_)).getOrElse(v)))
+        case Results(nots, time, _, _) if nots.nonEmpty =>
+          beDriftPref.mutate(v => time.map(clock.instant.until(_)).getOrElse(v))
+            .flatMap(_ => storeNotifications(nots))
+            .flatMap(_ => idPref().flatMap(syncHistory))
+        case Results(_, time, _, _) =>
+          beDriftPref.mutate(v => time.map(clock.instant.until(_)).getOrElse(v))
       }
 
-    if (fetchInProgress.isCompleted) {
-      verbose(l"Sync history in response to $source")
-      fetchInProgress = idPref().flatMap(syncHistory)
-    }
-    fetchInProgress
+    verbose(l"Sync history in response to $source")
+    idPref().flatMap(syncHistory)
   }
 }
 
